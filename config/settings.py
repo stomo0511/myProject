eb912ad06c630ee558272fdee578187243472f98
Tomohiro Suzuki <stomo@yamanashi.ac.jp>
2024-12-13 15:53:27 +0900

    statirroot 変更

--- config/settings.py
+++ config/settings.py
@@ -123,12 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-    # os.path.join(BASE_DIR, 'static'),
-#     # str(Path(BASE_DIR) / 'static'),
-)
+# STATICFILES_DIRS = (
+#     BASE_DIR / 'static',
+#     # os.path.join(BASE_DIR, 'static'),
+# #     # str(Path(BASE_DIR) / 'static'),
+# )
 # STATICFILES_DIRS = ['static']
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
