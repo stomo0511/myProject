91b2ec03e8105a2f2bfd9d574fe7ec3c82a7bf30
Tomohiro Suzuki <stomo@yamanashi.ac.jp>
2024-12-13 15:35:26 +0900

    20241213

--- config/settings.py
+++ config/settings.py
@@ -123,11 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (
-#     # os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+    # os.path.join(BASE_DIR, 'static'),
 #     # str(Path(BASE_DIR) / 'static'),
-# )
-STATICFILES_DIRS = ['static']
+)
+# STATICFILES_DIRS = ['static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
